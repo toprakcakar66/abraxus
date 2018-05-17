c53c4992cf15e073aee35dd6609903f025993666
toprakcakar66 <39004667+toprakcakar66@users.noreply.github.com>
2018-05-18 00:10:12 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -16,13 +16,6 @@ client.on('message', message => {
 });
 
 client.on('message', message => {
-  if (message.content === prefix + 'yardım') {
-    message.channel.send('```Selam ben ABRAXUS buda komutlarım.\n a?yardım :Tüm komutları atar.\n a?son video :Son videoyu atar.\n a?abone ol :Abraxus Youtube kanalını atar.\n a?yayın :Yayın linkini atar.\n ABRAXUS botumuzun komutları bu kadardı\n Sende bot yaptırmak istiyorsan bana ulaş\nToprak T#9528```');
-  }  
-});
-
-
-client.on('message', message => {
   if (message.content === 'baban') {
     message.channel.send('Benim babam Toprak');
   }  
