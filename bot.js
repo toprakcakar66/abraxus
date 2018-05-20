4413f813670c30ecf10c3473e15986be8a998d69
toprakcakar66 <39004667+toprakcakar66@users.noreply.github.com>
2018-05-20 22:03:30 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -20,7 +20,7 @@ client.on('message', message => {
 });
 
 client.on('message', message => {
-  if (message.content ==='sa') {
+  if (message.content.toLowerCase() ==='sa') {
     message.channel.send('Bot not editable.');
   }  
 });
