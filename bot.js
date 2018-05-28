5ff203273b824e17cc0ae5d531176e786c2dc61c
toprakcakar66 <39004667+toprakcakar66@users.noreply.github.com>
2018-05-28 21:11:49 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -62,35 +62,4 @@ client.on('message', message => {
   }  
 });
 
-client.on('message', message => {
-  if (message.author.bot) return;
-  if (!message.content.startsWith(ayarlar.prefix)) return;
-
-  let command = message.content.split(' ')[0];
-  command = command.slice(ayarlar.prefix.length);
-
-  let args = message.content.split(' ').slice(1);
-
-  if (command === 'topla') {
-    let numArray = args.map(n=> parseInt(n));
-    let total = numArray.reduce( (p, c) => p+c);
-    message.channel.sendMessage(`${total}`);
-  }
-  if (command === 'çıkar') {
-    let numArray = args.map(n=> parseInt(n));
-    let total = numArray.reduce( (p, c) => p-c);
-    message.channel.sendMessage(`${total}`);
-  }
-  if (command === 'çarp') {
-    let numArray = args.map(n=> parseInt(n));
-    let total = numArray.reduce( (p, c) => p*c);
-    message.channel.sendMessage(`${total}`);
-  }
-  if (command === 'böl') {
-    let numArray = args.map(n=> parseInt(n));
-    let total = numArray.reduce( (p, c) => p/c);
-    message.channel.sendMessage(`${total}`);
-  }
-});
-
 client.login('NDI5MzQ1NzA4ODUyNzcyODc0.Dd-Bqw.d_M0hk8k5ICGR4_PgqG38DNd798');
