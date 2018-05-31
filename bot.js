f8ae66e3f79003a76c8d3e0e12329b3f5aaa7e5d
toprakcakar66 <39004667+toprakcakar66@users.noreply.github.com>
2018-05-31 17:35:41 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -46,7 +46,7 @@ client.on('message', msg => {
  });   
 
     client.on('message', msg => {
-  if (msg.content.startsWith(prefix + "yazdır")) {
+  if (msg.content.startsWith(prefix + "yaz")) {
     if (msg.channel.type !== "dm"){
     let mesaj = msg.content.substring(2 + 3);
     msg.delete (msg.content == 'yaz' + mesaj)
