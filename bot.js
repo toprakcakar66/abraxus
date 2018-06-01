9d83be02b2768bc16731af5392f8760088605d4e
toprakcakar66 <39004667+toprakcakar66@users.noreply.github.com>
2018-06-01 14:25:05 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,20 +1,125 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
-var prefix = 'a?'
+var prefix = "ae!";
+var owner = "427061793123205131";
 
 client.on('ready', () => {
-  console.log(`[BOT] ${client.user.tag} ADI İLE GİRİŞ YAPTI!`);
+  client.user.setGame(`${prefix}komutlar|ABONE OL`);
+  console.log(`Logged in as ${client.user.tag}!`);
 });
 
-client.on('ready', () => {
-  client.user.setStatus("WATCHING"); // Aynı zamanda STREAMING: YAYINDA: WATCHING: İZLİYOR: DND: RAHATSIZ ETMEYİN GİBİ TERİMLERDE VARDIR
-  client.user.setActivity('1 Haziranda yeni kanal açılıyor', { type: "WATCHING"});  // Aynı zamanda STREAMING: YAYINDA: WATCHING: İZLİYOR: DND: RAHATSIZ ETMEYİN GİBİ TERİMLERDE VARDIR
-})
+client.on('message', msg => {
+  if (msg.content === prefix + "komutlar") {
+   const embed = new Discord.RichEmbed()
+   .addField(`Selam ben ${client.user.username}`, "Buda komutlarım")
+
+   .addField(`${prefix}aboneol`, `Adil ESER Youtube kanalının linkini atar.`)
+
+   .addField(`${prefix}davulcusözü`, `Rastegele bir davulcu sözü atar.`)
+
+   .addField(`${prefix}manisöyle`, `Rastgele bir mani sözü atar.`)
+
+   .addField(`${prefix}rastgelevideo`, `Adil ESER'in videolarından birisini atar.`)
+
+   .addField(`${prefix}sonvideo`, `Adil ESER'in atmış olduğu en son vidoyu atar.`)
+
+   .addField(`${prefix}yayın`, `Yayın var mı yok mu onu söyler.`)
+
+   .setColor(0xff0000)
+
+   return msg.channel.send(embed)
+   }
+});
+
+client.on('message', msg => {
+  if (msg.content.toLocaleLowerCase() === "sa") {
+    msg.reply(`**Aleyküm Selam**`)
+  }
+  if (msg.content.toLocaleLowerCase() === prefix + "sonvideo") {
+      msg.channel.sendMessage('https://www.youtube.com/watch?v=iTKF3zrVwag')
+  }
+  if (msg.content.toLocaleLowerCase() === prefix + "aboneol") {
+    msg.channel.sendMessage('https://www.youtube.com/channel/UC0XoYr1k-1XcvqpYfBEPmjg')
+  }
+  if (msg.content === prefix + "rastgelevideo") {
+    function get_random(list) {
+        return list[Math.floor((Math.random() * list.length))];
+    }
+
+    var mesaj1 = "https://www.youtube.com/watch?v=iTKF3zrVwag"
+    var mesaj2 = "https://www.youtube.com/watch?v=JBYk4J02tcE"
+    var mesaj3 = "https://www.youtube.com/watch?v=EJIXW7ezfvo"
+    var mesaj4 = "https://www.youtube.com/watch?v=Vfc76YDdc2I"
+    var mesaj5 = "https://www.youtube.com/watch?v=E1fsTL4fQzI"
+    var mesaj6 = "https://www.youtube.com/watch?v=E1fsTL4fQzI"
+    var mesaj7 = "https://www.youtube.com/watch?v=QJLyT39275o"
+
+    let mesaj = [mesaj1, mesaj2, mesaj3, mesaj4, mesaj5, mesaj6, mesaj7]
+    let sonuc = get_random(mesaj)
+
+    let embed = new Discord.RichEmbed()
+        .setColor("RANDOM")
+        .setTitle("Rastgele video")
+        .setDescription(sonuc)
+    return msg.channel.send(embed)
+}
+if (msg.content === prefix + "davulcusözü") {
+  function get_random(list) {
+      return list[Math.floor((Math.random() * list.length))];
+  }
+
+  var mani1 = "Sofrada fakir olsun,Tabağı çukur olsun,Karnı doyduktan sonra,Duayı okur olsun"
+  var mani2 = "Karşıma fener geldi,Aklıma neler geldi,Börek bekledim ama,Sofraya döner geldi"
+  var mani3 = "Bu aya hürmet gerek,Nimete şükür gerek,Mübarek Ramazan'da,Hakka ibadet gerek"
+  var mani4 = "Ahmet ağa uyursun,Uykularda ne bulursun,Kalk al abdest,kıl namaz,Mutluluğu bulursun"
+  var mani5 = "Hakk'ın bize ihsanısın,Hem ayların sultanısın,Sen bir saadet kanısın,Ey mahı sultan merhaba"
+  var mani6 = "Akşamdan pilavı pişirdim,Gene karnımı şişirdim,Çok mani diyecektim ama,Defteri yolda düşürdüm"
+
+  let mani = [mani1, mani2, mani3, mani4, mani5, mani6]
+  let sonuc = get_random(mani)
+
+  let embed = new Discord.RichEmbed()
+      .setColor("RANDOM")
+      .setTitle("Davulcu Sözü")
+      .setDescription(sonuc)
+  return msg.channel.send(embed)
+}
+if (msg.content === prefix + "manisöyle") {
+  function get_random(list) {
+      return list[Math.floor((Math.random() * list.length))];
+  }
+
+  var mani1 = "Ayakkabı giyerim üstü güzel olursa,Kaynana severim oğlu güzel olursa"
+  var mani2 = "Trenin penceresi,Evin çerçevesi,Ebru hocayı sevmeyen,Olsun bulaşık tenceresi"
+  var mani3 = "Ciğer verdim kediye,Aldım sana hediye,Herkes beni kıskanır,Sen severim diye"
+  var mani4 = "Amasya'nın elması,Hoştur yari sarılması,Kadehle olmaz bu iş,Doldur bakır tası."
+  var mani5 = "Dolu vurdu bağıma,Yel attı yaptağını,Korkarım garip ölem,El atar yaprağımı"
+  var mani6 = "Irmaktan geçemiyom,Az doldur içemiyom,Üç beş dostu görünce,Yazmadan edemiyom"
+  var mani7 = "Bir dalda iki elma,Birini al birini alma,Alnına yazılmışım,İster al ister alma"
+  var mani8 = "Denizde alabalık,Yüreğim sana yanık,İsterim konuşayım,Mahalle kalabalık"
+  var mani9 = "Kınalı parmak ucu,Benim yarim koruyucu,Namaz kılmazsa,Kabul olmaz orucu"
+  var mani10 = "Gökte yıldız bi sıra,Yarim gitti Mısır'a,Yarim keklik ben şahin,Giderim ardı sıra"
+  var mani11 = "Maydanoz demet demet,Yarimin adı Mehmet,Mehmet benim olursa,Cehennem olur cennet"
+  var mani12 = "Bayram abi gibi evde misin,Tilki gibi inde misin,Sana mani söylersem bana,Bahşiş verir misin"
+
+  let mani = [mani1, mani2, mani3, mani4, mani5, mani6, mani7, mani8, mani9, mani10, mani11, mani12]
+  let sonuc = get_random(mani)
+
+  let embed = new Discord.RichEmbed()
+      .setColor("RANDOM")
+      .setTitle("Mani")
+      .setDescription(sonuc)
+  return msg.channel.send(embed)
+}
+ if (msg.content.toLowerCase() === prefix + "yayın") {
+   msg.reply('şu anda yayın yok.Lütfen daha sonra tekrar dene.')
+ }
+});
 
 client.on('guildMemberAdd', member => {
   let guild = member.guild;
-  let joinRole = guild.roles.find('name', 'BOTLA'); // Burada girişte verilcek rolu seçelim.
+  let joinRole = guild.roles.find('name', 'Üye'); // Burada girişte verilcek rolu seçelim.
   member.addRole(joinRole); // seçtiğimiz rolu verelim.
 
   const channel = member.guild.channels.find('name', 'gelen-kisiler'); // burda ise kanalı belirleyelim hangi kanala atsın ben mod-log dedim.
@@ -23,71 +128,25 @@ client.on('guildMemberAdd', member => {
   .setColor('RANDOM')
   .setAuthor(member.user.username, member.user.avatarURL)
   .setThumbnail(member.user.avatarURL)
-  .setTitle('Hoşgeldin beş gittin bro', client.users.size, true)
+  .setTitle('Kardeşim sunucumuza hoşgeldin.GÜZEL VAKİT GEÇİRMEN DİLEKLERİYLE')
   .setTimestamp()
   channel.sendEmbed(embed); // belirlediğimiz kanala mesaj gönderelim.
 });
 
-client.on('message', msg => {
-  if (msg.content.toLowerCase() === prefix + 'temizle 100') {
-    msg.channel.bulkDelete(100);
-    msg.channel.sendMessage("100 adet mesaj silindi!");
-  }
+client.on('guildMemberAdd', member => {
+  let guild = member.guild;
+  let joinRole = guild.roles.find('name', 'Muted'); // Burada girişte verilcek rolu seçelim.
+  member.addRole(joinRole); // seçtiğimiz rolu verelim.
+
+  const channel = member.guild.channels.find('name', 'gelen-'); // burda ise kanalı belirleyelim hangi kanala atsın ben mod-log dedim.
+  if (!channel) return;
+  const embed = new Discord.RichEmbed()
+  .setColor('RANDOM')
+  .setAuthor(member.user.username, member.user.avatarURL)
+  .setThumbnail(member.user.avatarURL)
+  .setTitle('Kardeşim sunucumuza hoşgeldin.GÜZEL VAKİT GEÇİRMEN DİLEKLERİYLE')
+  .setTimestamp()
+  channel.sendEmbed(embed); // belirlediğimiz kanala mesaj gönderelim.
 });
 
- client.on('message', msg => {
-   if (msg.content === prefix + "avatarım") {
-       const embed = new Discord.RichEmbed()
-           .setColor(0x85cfff)
-           .setImage(msg.author.avatarURL)
-           .setTitle(msg.author.tag, "İşte avatarın")
-        return msg.channel.send(embed)
-   }
- });   
-
- client.on('message', msg => {
-  if (msg.content.startsWith(prefix + "yaz")) {
-    if (msg.channel.type !== "dm"){
-    let mesaj = msg.content.substring(2 + 3);
-    msg.delete (msg.content == 'yaz' + mesaj)
-    let embed = new Discord.RichEmbed()
-    .setColor("RANDOM")
-       .setDescription(mesaj)
-return msg.channel.send({embed})}
-
-    }
-  });
-
-
-client.on('message', msg => {
-  if (msg.content.toLowerCase() === prefix + 'temizle 10') {
-    msg.channel.bulkDelete(10);
-    msg.channel.sendMessage("10 adet mesaj silindi!");
-  }
-});  
-
-client.on('message', message => {
-  if (message.content === prefix + 'son video') {
-    message.channel.sendMessage('https://www.youtube.com/watch?v=3HDxxnuSB-8');
-  }  
-});
-
-client.on('message', message => {
-  if (message.content.toLowerCase() ==='sa') {
-    message.channel.send('**Küçük Toprak yetkisini istiyor çok mu ba.**');
-  }  
-});
-
-client.on('message', message => {
-  if (message.content === prefix + 'abone ol') {
-    message.channel.send('https://www.youtube.com/channel/UCCMnMpRipcSwt865ZEFDong');
-  }  
-});
-
-client.on('message', message => {
-  if (message.content === prefix + 'yayın') {
-    message.reply('Şu anda yayında değil.Daha sonra tekrar yaz.');
-  }  
-});
-
-client.login('NDI5MzQ1NzA4ODUyNzcyODc0.Dd-Bqw.d_M0hk8k5ICGR4_PgqG38DNd798');
+client.login('NDUyMDQ1MjE0NDIxNjE0NTkz.DfKncQ.2UksOTXfVzAvQ2PBHPWIWN3Dxdc');
